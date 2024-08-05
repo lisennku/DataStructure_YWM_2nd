52d5ce001aa5dedf394a4727c647d218217f09bb
Lisen Zhang <245062522@qq.com>
2024-08-05 13:11:06 +0800

    chap 1 basic knowledge

---
+++ basic.h
@@ -0,0 +1,17 @@
+//
+// Created by lisen on 24-8-5.
+//
+
+#ifndef BASIC_H
+#define BASIC_H
+
+// macro definition
+// function status code
+#define OK 1
+#define ERROR 0
+#define OVERFLOW -2
+
+// data type of return value for a function, indicates the status
+typedef int Status;
+
+#endif //BASIC_H
