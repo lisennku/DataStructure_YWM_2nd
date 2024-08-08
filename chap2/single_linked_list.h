8977b675a85a75428ae61e28bca261d33cc2412a
Lisen Zhang <245062522@qq.com>
2024-08-08 17:51:43 +0800

    single linked list test1

--- chap2/single_linked_list.h
+++ chap2/single_linked_list.h
@@ -5,4 +5,13 @@
 #ifndef SINGLE_LINKED_LIST_H
 #define SINGLE_LINKED_LIST_H
 
+typedef int Item;
+struct node {
+	Item item;
+	struct node * next;
+};
+
+typedef struct node Node;
+typedef struct node * Single_linked_list;
+
 #endif //SINGLE_LINKED_LIST_H
--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include "chap2/single_linked_list.h"
+#include <stdlib.h>
 
 int main(void)
 {
-    printf("Hello, World!\n");
-    return 0;
+    Single_linked_list sll;
+    Node * head_node = (Node *) malloc(sizeof(Node));
+    head_node->next = NULL;
+    sll = head_node;
+    printf("sll points to %p\n", sll);
+    printf("sll's address is %p\n", &sll);
 }
