79723decba877b1782aab22cf49544c246a56f9c
Lisen Zhang <245062522@qq.com>
2024-10-16 17:24:27 +0800

    leetcode-75

---
+++ Leetcode/LeetCode75/238.product-of-array-except-self.c
@@ -0,0 +1,92 @@
+//
+// Created by lisen on 24-10-16.
+//
+// https://leetcode.cn/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=leetcode-75
+// 给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+// 题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+// 请 不要使用除法，且在 O(n) 时间复杂度内完成此题。
+
+/*
+ * 使用前缀积和后缀积
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int * prefix_product(int * nums, int numSize, int * pre_len) {
+    int * pre = (int *) calloc(numSize, sizeof(int));
+    if(pre == NULL)
+        exit(EXIT_FAILURE);
+
+    pre[0] = nums[0];
+
+    int i = 1;
+    while(i < numSize) {
+        pre[i] = nums[i] * pre[i-1];
+        i++;
+    }
+
+    *pre_len = numSize;
+    return pre;
+}
+int * postfix_product(int * nums, int numSize, int * pst_len) {
+    int * post = (int *) calloc(numSize, sizeof(int));
+    if(post == NULL)
+        exit(EXIT_FAILURE);
+
+    post[numSize-1] = nums[numSize-1];
+
+    int i = numSize - 2;
+    while(i >= 0) {
+        post[i] = nums[i] * post[i+1];
+        i--;
+    }
+
+    *pst_len = numSize;
+    return post;
+}
+
+int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+    int * res = (int *) calloc(numsSize, sizeof(int));
+    if(res == NULL)
+        exit(EXIT_FAILURE);
+
+    int pre_len;
+    int pst_len;
+    int * pre = prefix_product(nums, numsSize, &pre_len);
+    int * pst = postfix_product(nums, numsSize, &pst_len);
+
+    for(int i = 0; i < numsSize; i++) {
+        int pre_value = (i == 0 ? 1 : pre[i-1]);
+        int pst_value = (i == numsSize-1 ? 1 : pst[i+1]);
+
+        res[i] = pre_value * pst_value;
+    }
+    *returnSize = numsSize;
+    return res;
+}
+
+int main() {
+    int nums[] = {2, 3, 4, 5, 6};
+    int numSize = 5;
+    int returnSize;
+    // int pre_len;
+    // int pst_len;
+    //
+    // int * pre = prefix_product(nums, numSize, &pre_len);
+    // int * pst = postfix_product(nums, numSize, &pst_len);
+    //
+    // for(int i = 0; i < pre_len; i++)
+    //     printf("%d ", pre[i]);
+    // printf("\n");
+    // for(int i = 0; i < pst_len; i++)
+    //     printf("%d ", pst[i]);
+
+    int * res = productExceptSelf(nums, numSize, &returnSize);
+    for(int i = 0; i < numSize; i++)
+        printf("%d ", res[i]);
+
+    putchar('\n');
+
+    return 0;
+}
---
+++ Leetcode/LeetCode75/334.increasing-triplet-subsequence.c
@@ -0,0 +1,88 @@
+//
+// Created by lisen on 24-10-16.
+//
+// https://leetcode.cn/problems/increasing-triplet-subsequence/description/?envType=study-plan-v2&envId=leetcode-75
+// 给你一个整数数组 nums ，判断这个数组中是否存在长度为 3 的递增子序列。
+//
+// 如果存在这样的三元组下标 (i, j, k) 且满足 i < j < k ，使得 nums[i] < nums[j] < nums[k] ，返回 true ；否则，返回 false 。
+
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+/*
+ * 回溯算法会在用例76时超时
+ */
+
+// void backtrack(
+//     int * state,
+//     int state_size,
+//     bool * flag,
+//     int * nums,
+//     int numsSize,
+//     int start
+// ) {
+//     if(*flag == false) {
+//         if(state_size == 3) {
+//             *flag = true;
+//             return ;
+//         }
+//
+//         for(int i = start; i < numsSize; i++) {
+//             if(state_size != 0 && nums[i] <= state[state_size-1])
+//                 continue;
+//
+//             state[state_size] = nums[i];
+//             state_size ++;
+//             backtrack(state, state_size, flag, nums, numsSize, i + 1);
+//             if(*flag == false)
+//                 state_size--;
+//             else
+//                 return ;
+//         }
+//
+//
+//     }
+//
+// }
+//
+// bool increasingTriplet(int* nums, int numsSize) {
+//     bool flag = false;
+//     int state[3];
+//     backtrack(state, 0, &flag, nums, numsSize, 0);
+//     return flag;
+// }
+
+bool increasingTriplet(int* nums, int numsSize) {
+    if(numsSize < 3)
+        return false;
+
+    int first = nums[0];
+    int second = INT_MAX;
+
+    for(int i = 1; i < numsSize; i++) {
+        int num = nums[i];
+        if(num > second)
+            return true;
+        if(num > first)
+            second = num;
+        else
+            first = num;
+    }
+
+    return false;
+}
+
+
+int main() {
+    // int nums[] = {2, 10, 9, 3, 7, 4, 6};
+    // int nums[] = {5, 4, 3, 2, 1};
+    // int nums[] = {1, 2, 3, 4, 5};
+    int nums[] = {20,100,10,12,5,13};
+    int numsSize = sizeof(nums) / sizeof(int);
+
+    bool b = increasingTriplet(nums, numsSize);
+    printf("%d\n", b);
+
+    return 0;
+}
