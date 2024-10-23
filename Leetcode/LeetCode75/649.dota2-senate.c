e9dc36145a735e5007cf797f24b92c8ba58af82a
Lisen Zhang <245062522@qq.com>
2024-10-23 09:24:11 +0800

    leetcode-75 queue part

---
+++ Leetcode/LeetCode75/649.dota2-senate.c
@@ -0,0 +1,19 @@
+//
+// Created by lisen on 24-10-22.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+char* predictPartyVictory(char* senate) {
+    /*
+     * 使用队列 遍历字符串
+     * 
+     */
+    char * winner_r = "Radiant";
+    char * winner_d = "Dire";
+
+
+
+
+}
